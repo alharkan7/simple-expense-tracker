a0bd9c5a0e153584e9808302a411ffc0e8868f91
Al <ali.al81@ui.ac.id>
2024-09-30 12:21:50 +0700

    Fixed unused-vars ESLint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from 'next/head';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
